d9742eb6cf28ddfcb70e4431feb2b2b80bb1bf5a
burcusakir9 <burcusakir99@gmail.com>
2026-07-26 21:39:20 +0300

    [FIX] Update linearized model of tugboat. Now it depends on inputs too.

--- tugboat3d_linearized.m
+++ tugboat3d_linearized.m
@@ -1,25 +1,59 @@
-function [A, B] = tugboat3d_linearized(x_eq, u_eq)
-% Analytical Jacobian of tugboat3d around (x_eq, u_eq)
+function [A_c, B_c] = tugboat3d_linearized(x_bar, u_bar)
+%TUGBOAT3D_LINEARIZED Continuous-time analytical linearization.
 %
-% x = [X; Y; psi; ub; vb; r]
-% u = [FL; FR]
+% Linearizes tugboat3d around the nominal state-input pair:
+%
+%   x_bar = [X; Y; psi; ub; vb; r]
+%   u_bar = [FL; FR]
+%
+% Returns the continuous-time error-state model:
+%
+%   delta_xdot = A_c*delta_x + B_c*delta_u
+%
+% where:
+%
+%   delta_x = x - x_bar
+%   delta_u = u - u_bar
+%
+% The matrices can subsequently be discretized for MPC.
 
-    X   = x_eq(1);
-    Y   = x_eq(2);
-    psi = x_eq(3);
-    ub  = x_eq(4);
-    vb  = x_eq(5);
-    r   = x_eq(6);
+    % Ensure column vectors
+    x_bar = x_bar(:);
+    u_bar = u_bar(:);
 
-    FL = u_eq(1);
-    FR = u_eq(2);
+    % Validate dimensions
+    if numel(x_bar) ~= 6
+        error('x_bar must contain 6 elements.');
+    end
 
-    % Parameters
+    if numel(u_bar) ~= 2
+        error('u_bar must contain 2 elements.');
+    end
+
+    % State at the linearization point
+    psi = x_bar(3);
+    ub  = x_bar(4);
+    vb  = x_bar(5);
+    r   = x_bar(6);
+
+    % u_bar does not currently affect A_c or B_c because the nonlinear
+    % model is affine in FL and FR. It is retained in the interface so that
+    % the linearization point is represented consistently.
+    %#ok<NASGU>
+
+    %%%%%%%%%%%%%%%%%%%%%%%
+    % Model parameters    %
+    %%%%%%%%%%%%%%%%%%%%%%%
+
     m  = 10.2;
     Iz = 0.63994;
 
     beam = 0.29;
-    d = beam/2;
+
+    % This assumes that each thrust line is located at beam/2 from the
+    % vessel centerline. Replace with the actual CG-to-thrust-line distance
+    % when available.
+    d = beam / 2.0;
 
     Xu    = -5.76909;
     Xuu   = -2.17161;
@@ -36,73 +70,94 @@ function [A, B] = tugboat3d_linearized(x_eq, u_eq)
     Nrr   = -0.33077;
     Nrdot = -0.04531;
 
-    % Inertia matrix
-    M = [m - Xudot,      0,            0;
-         0,              m - Yvdot,   -Yrdot;
-         0,             -Yrdot,        Iz - Nrdot];
+    %%%%%%%%%%%%%%%%%%%%%%%
+    % Inertia matrix      %
+    %%%%%%%%%%%%%%%%%%%%%%%
+
+    % Assumes added-mass symmetry:
+    % Nvdot = Yrdot
+    M = [m - Xudot,  0.0,          0.0;
+         0.0,        m - Yvdot,   -Yrdot;
+         0.0,       -Yrdot,        Iz - Nrdot];
 
     m11 = M(1,1);
     m22 = M(2,2);
     m23 = M(2,3);
 
-    % ---------- Kinematics Jacobian ----------
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Continuous-time state Jacobian    %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    A_c = zeros(6,6);
+
     cpsi = cos(psi);
     spsi = sin(psi);
 
-    A = zeros(6,6);
-
     % Xdot = cos(psi)*ub - sin(psi)*vb
-    A(1,3) = -spsi*ub - cpsi*vb;
-    A(1,4) =  cpsi;
-    A(1,5) = -spsi;
+    A_c(1,3) = -spsi*ub - cpsi*vb;
+    A_c(1,4) =  cpsi;
+    A_c(1,5) = -spsi;
 
     % Ydot = sin(psi)*ub + cos(psi)*vb
-    A(2,3) =  cpsi*ub - spsi*vb;
-    A(2,4) =  spsi;
-    A(2,5) =  cpsi;
+    A_c(2,3) =  cpsi*ub - spsi*vb;
+    A_c(2,4) =  spsi;
+    A_c(2,5) =  cpsi;
 
     % psidot = r
-    A(3,6) = 1;
+    A_c(3,6) = 1.0;
 
-    % ---------- Dynamics Jacobian ----------
-    % F = tau + tau_h - C*nu
-    % nudot = M^{-1} F
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Dynamic part of state Jacobian    %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %
-    % With:
-    % F1 = FL+FR + Xu*ub + Xuu*|ub|ub + m22*vb*r + m23*r^2
-    % F2 =         Yv*vb + Yr*r + Yvv*|vb|vb - m11*ub*r
-    % F3 = d(FL-FR)+ Nv*vb + Nr*r + Nrr*|r|r - (m22-m11)*ub*vb - m23*ub*r
+    % nudot = M^(-1) F(nu,u)
+    %
+    % F1 = FL + FR
+    %      + Xu*ub + Xuu*abs(ub)*ub
+    %      + m22*vb*r + m23*r^2
+    %
+    % F2 = Yv*vb + Yr*r + Yvv*abs(vb)*vb
+    %      - m11*ub*r
+    %
+    % F3 = d*(FL-FR)
+    %      + Nv*vb + Nr*r + Nrr*abs(r)*r
+    %      - (m22-m11)*ub*vb
+    %      - m23*ub*r
 
-    dabs_ub = 2*abs(ub);
-    dabs_vb = 2*abs(vb);
-    dabs_r  = 2*abs(r);
+    % For g(q) = abs(q)*q:
+    %
+    % dg/dq = 2*abs(q)
+    dub_drag = 2.0 * abs(ub);
+    dvb_drag = 2.0 * abs(vb);
+    dr_drag  = 2.0 * abs(r);
 
     dF_dnu = zeros(3,3);
 
-    % Row 1 derivatives
-    dF_dnu(1,1) = Xu + Xuu*dabs_ub;
+    % F1 derivatives with respect to [ub, vb, r]
+    dF_dnu(1,1) = Xu + Xuu*dub_drag;
     dF_dnu(1,2) = m22*r;
-    dF_dnu(1,3) = m22*vb + 2*m23*r;
+    dF_dnu(1,3) = m22*vb + 2.0*m23*r;
 
-    % Row 2 derivatives
+    % F2 derivatives with respect to [ub, vb, r]
     dF_dnu(2,1) = -m11*r;
-    dF_dnu(2,2) = Yv + Yvv*dabs_vb;
+    dF_dnu(2,2) = Yv + Yvv*dvb_drag;
     dF_dnu(2,3) = Yr - m11*ub;
 
-    % Row 3 derivatives
+    % F3 derivatives with respect to [ub, vb, r]
     dF_dnu(3,1) = -(m22 - m11)*vb - m23*r;
     dF_dnu(3,2) = Nv - (m22 - m11)*ub;
-    dF_dnu(3,3) = Nr + Nrr*dabs_r - m23*ub;
+    dF_dnu(3,3) = Nr + Nrr*dr_drag - m23*ub;
 
-    A_nu = M \ dF_dnu;
+    A_c(4:6,4:6) = M \ dF_dnu;
 
-    A(4:6,4:6) = A_nu;
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Continuous-time input Jacobian    %
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    % ---------- Input Jacobian ----------
-    B_prop = [1, 1;
-              0, 0;
-              d, -d];
+    B_prop = [1.0,  1.0;
+              0.0,  0.0;
+              d,   -d];
 
-    B = zeros(6,2);
-    B(4:6,:) = M \ B_prop;
+    B_c = zeros(6,2);
+    B_c(4:6,:) = M \ B_prop;
 end
