5a8ba7e4dc5fe4cc0b6bd2df164e182ac290f8fc
burcusakir9 <burcusakir99@gmail.com>
2026-08-02 17:02:31 +0300

    [ADD] Added first order actyuator dynamics to the model.

--- tugboat3d.m
+++ tugboat3d.m
@@ -1,25 +1,33 @@
 function [xdot, U, M, B_prop] = tugboat3d(x, u)
-%TUGBOAT3D  3-DOF tugboat model obtained from
-% İzzet Kağan Erünsal Thesis
+%TUGBOAT3D  3-DOF tugboat model with first-order actuator dynamics
 %
 % State:
-%   x = [eta; nu] = [X; Y; psi; ub; vb; r]
+%   x = [eta; nu; F]
+%     = [X; Y; psi; ub; vb; r; F_L; F_R]
 %
 % Input:
-%   u = [F_L; F_R]
+%   u = [F_L_cmd; F_R_cmd]
 %
 % Model:
-%   eta_dot = J(psi) * nu
+%   eta_dot = J(psi)*nu
 %   M*nu_dot + C(nu)*nu + D(nu)*nu = tau
-%   tau = B_prop * input
+%   tau = B_prop*F
+%
+% Actuator model:
+%   T_L*F_L_dot + F_L = F_L_cmd
+%   T_R*F_R_dot + F_R = F_R_cmd
 
-    % Parameters 
+    % Parameters
     m  = 10.2;        % [kg] approximate boat mass
     Iz = 0.63994;     % [kg m^2] identified yaw inertia
 
     % Geometry
     beam = 0.29;      % [m]
     d = beam/2;       % assumed |y_thruster - y_CG| [m]
+
+    % Actuator time constants
+    T_L = 0.25;       % [s] left actuator time constant
+    T_R = 0.25;       % [s] right actuator time constant
 
     % Identified hydrodynamic derivatives
     Xu    = -5.76909;
@@ -38,25 +46,32 @@ function [xdot, U, M, B_prop] = tugboat3d(x, u)
     Nrdot = -0.04531;
 
     % States
-    psi = x(3);
-    ub  = x(4);
-    vb  = x(5);
-    r   = x(6);
-
-    FL = u(1);
-    FR = u(2);
-
     eta = x(1:3);
-    nu  = x(4:6); %nu = [ub; vb; r];
-    U  = hypot(ub, vb);
+    nu  = x(4:6);
+    F   = x(7:8);
 
-    % Thruster allocation: tau = [X_force; Y_force; N_moment]
+    psi = eta(3);
 
+    ub = nu(1);
+    vb = nu(2);
+    r  = nu(3);
+
+    FL = F(1);
+    FR = F(2);
+
+    % Commanded actuator forces
+    FL_cmd = u(1);
+    FR_cmd = u(2);
+
+    U = hypot(ub, vb);
+
+    % Thruster allocation
     B_prop = [1, 1;
               0, 0;
               d, -d];
 
-    tau = B_prop * [FL; FR];
+    % Actual actuator forces are applied to the vessel
+    tau = B_prop * F;
 
     % 3-DOF inertia matrix
     M = [m - Xudot,      0,            0;
@@ -68,30 +83,34 @@ function [xdot, U, M, B_prop] = tugboat3d(x, u)
     m22 = M(2,2);
     m23 = M(2,3);
 
-    C = [0,                0,  -(m22*vb + m23*r);
-         0,                0,              m11*ub;
-         m22*vb + m23*r,  -m11*ub,              0];
+    C = [0,                0, -(m22*vb + m23*r);
+         0,                0,             m11*ub;
+         m22*vb + m23*r, -m11*ub,               0];
 
     % Linear and nonlinear damping matrix
     D = [-Xu - Xuu*abs(ub),                  0,                 0;
                           0, -Yv - Yvv*abs(vb),               -Yr;
                           0,                -Nv, -Nr - Nrr*abs(r)];
 
-
-
-    % Dynamics
+    % Vessel dynamics
     nu_dot = M \ (tau - C*nu - D*nu);
 
     % Kinematics
     J = [cos(psi), -sin(psi), 0;
          sin(psi),  cos(psi), 0;
-            0,         0, 1];
+                0,         0, 1];
 
     eta_dot = J * nu;
 
+    % First-order actuator dynamics
+    FL_dot = (FL_cmd - FL) / T_L;
+    FR_dot = (FR_cmd - FR) / T_R;
+
+    F_dot = [FL_dot;
+             FR_dot];
+
     % Complete state derivative
     xdot = [eta_dot;
-            nu_dot];
+            nu_dot;
+            F_dot];
 end
-
-
--- tugboat3d_linearized.m
+++ tugboat3d_linearized.m
@@ -2,8 +2,10 @@ function [A_c, B_c] = tugboat3d_linearized(x_bar, u_bar)
 %TUGBOAT3D_LINEARIZED Continuous-time analytical linearization of tugboat3d
 %
 % Linearization point:
-%   x_bar = [eta_bar; nu_bar] = [X; Y; psi; ub; vb; r]
-%   u_bar = [F_L; F_R]
+%   x_bar = [eta_bar; nu_bar; F_bar]
+%         = [X; Y; psi; ub; vb; r; F_L; F_R]
+%
+%   u_bar = [F_L_cmd; F_R_cmd]
 %
 % Linear error-state model:
 %   delta_xdot = A_c*delta_x + B_c*delta_u
@@ -13,8 +15,8 @@ function [A_c, B_c] = tugboat3d_linearized(x_bar, u_bar)
     u_bar = u_bar(:);
 
     % Validate dimensions
-    if numel(x_bar) ~= 6
-        error('x_bar must contain 6 elements.');
+    if numel(x_bar) ~= 8
+        error('x_bar must contain 8 elements.');
     end
 
     if numel(u_bar) ~= 2
@@ -28,6 +30,10 @@ function [A_c, B_c] = tugboat3d_linearized(x_bar, u_bar)
     % Geometry
     beam = 0.29;
     d = beam/2;
+
+    % Actuator time constants
+    T_L = 0.25;
+    T_R = 0.25;
 
     % Identified hydrodynamic derivatives
     Xu    = -5.76909;
@@ -48,11 +54,16 @@ function [A_c, B_c] = tugboat3d_linearized(x_bar, u_bar)
     % Nominal states
     eta_bar = x_bar(1:3);
     nu_bar  = x_bar(4:6);
+    F_bar   = x_bar(7:8);
 
     psi = eta_bar(3);
-    ub  = nu_bar(1);
-    vb  = nu_bar(2);
-    r   = nu_bar(3);
+
+    ub = nu_bar(1);
+    vb = nu_bar(2);
+    r  = nu_bar(3);
+
+    FL = F_bar(1);
+    FR = F_bar(2);
 
     % Thruster allocation
     B_prop = [1, 1;
@@ -65,7 +76,7 @@ function [A_c, B_c] = tugboat3d_linearized(x_bar, u_bar)
          0,             -Yrdot,        Iz - Nrdot];
 
     % Continuous-time state Jacobian
-    A_c = zeros(6,6);
+    A_c = zeros(8,8);
 
     cpsi = cos(psi);
     spsi = sin(psi);
@@ -85,7 +96,7 @@ function [A_c, B_c] = tugboat3d_linearized(x_bar, u_bar)
 
     % Dynamic Jacobian
     %
-    % nu_dot = M \ (tau - C(nu)*nu - D(nu)*nu)
+    % nu_dot = M \ (B_prop*F - C(nu)*nu - D(nu)*nu)
     m11 = M(1,1);
     m22 = M(2,2);
     m23 = M(2,3);
@@ -109,7 +120,20 @@ function [A_c, B_c] = tugboat3d_linearized(x_bar, u_bar)
 
     A_c(4:6,4:6) = M \ dF_dnu;
 
+    % Effect of actual actuator forces on vessel dynamics
+    A_c(4:6,7:8) = M \ B_prop;
+
+    % First-order actuator state dynamics
+    %
+    % F_L_dot = (F_L_cmd - F_L)/T_L
+    % F_R_dot = (F_R_cmd - F_R)/T_R
+    A_c(7,7) = -1/T_L;
+    A_c(8,8) = -1/T_R;
+
     % Continuous-time input Jacobian
-    B_c = zeros(6,2);
-    B_c(4:6,:) = M \ B_prop;
+    B_c = zeros(8,2);
+
+    % Commanded forces affect the actuator states
+    B_c(7,1) = 1/T_L;
+    B_c(8,2) = 1/T_R;
 end
